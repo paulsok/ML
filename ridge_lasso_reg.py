e3463f7c742594ce0484ac6bca0cdcadb7567400
Paul <paul.s.falke@gmail.com>
2021-01-28 01:08:31 +0500

    regularization L1&L2 added

--- ridge_lasso_reg.py
+++ ridge_lasso_reg.py
@@ -1,8 +1,7 @@
-
-import matplotlib.pyplot as plt
 import numpy as np
-from sklearn.base import BaseEstimator, TransformerMixin
+import matplotlib.pyplot as plt
 from sklearn.pipeline import make_pipeline
+from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.linear_model import LinearRegression, Ridge, Lasso
 
 
@@ -16,6 +15,7 @@ class GaussianFeatures(BaseEstimator, TransformerMixin):
         arg = (x - y) / width
         return np.exp(-0.5 * np.sum(arg ** 2, axis))
 
+    # Gaussian basis functions approximation
     def fit(self, X, y=None):
         self.centers_ = np.linspace(X.min(), X.max(), self.N)
         self.width_ = self.width_factor * (self.centers_[1] - self.centers_[0])
@@ -26,17 +26,39 @@ class GaussianFeatures(BaseEstimator, TransformerMixin):
                                  self.width_, axis=1)
 
 
-xfit = np.linspace(0, 10, 1000)
+def basis_plot(model, title=None):
+    fig, ax = plt.subplots(2, sharex=True)
+
+    model.fit(x[:, np.newaxis], y)
+
+    ax[0].scatter(x, y)
+    ax[0].plot(xfit, model.predict(xfit[:, np.newaxis]))
+    ax[0].set(xlabel='x', ylabel='y', ylim=(-1.5, 1.5))
+    if title:
+        ax[0].set_title(title)
+    ax[1].plot(model.steps[0][1].centers_, model.steps[1][1].coef_)
+    ax[1].set(xlabel='basis location', ylabel='coefficient', xlim=(0, 10))
+    plt.show()
+
 
 rng = np.random.RandomState(1)
 x = 10 * rng.rand(50)
 y = np.sin(x) + 0.1 * rng.randn(50)
+xfit = np.linspace(0, 10, 1000)
 
-gauss_model = make_pipeline(GaussianFeatures(20),
-                            LinearRegression())
+gauss_model = make_pipeline(GaussianFeatures(20), LinearRegression())
 gauss_model.fit(x[:, np.newaxis], y)
 yfit = gauss_model.predict(xfit[:, np.newaxis])
+
 plt.scatter(x, y)
 plt.plot(xfit, yfit)
 plt.xlim(0, 10)
 plt.show()
+
+# L2
+model = make_pipeline(GaussianFeatures(30), Ridge(alpha=0.1))
+basis_plot(model, title='Ridge Regression')
+
+# L1
+model = make_pipeline(GaussianFeatures(30), Lasso(alpha=0.001))
+basis_plot(model, title='Lasso Regression')
