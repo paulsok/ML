998f5b7c8b4f4a9b6c9077b3645f1a210af3409e
Paul <paul.s.falke@gmail.com>
2021-02-06 22:14:52 +0300

    PCA added

---
+++ PCA.py
@@ -0,0 +1,10 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# random data
+rng = np.random.RandomState(1)
+X = np.dot(rng.rand(2, 2), rng.randn(2, 200)).T
+plt.scatter(X[:, 0], X[:, 1])
+plt.axis('equal')
+plt.show()
