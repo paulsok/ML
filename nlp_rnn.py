b441077240e6c3f87fd08e9deadaed8b24fb5948
paulsok <paul.s.falke@gmail.com>
2022-06-25 23:05:58 +0300

    pep style fix

--- nlp_rnn.py
+++ nlp_rnn.py
@@ -3,7 +3,7 @@ import numpy as np
 import tensorflow as tf
 
 
-shakespeare_url = "https://homl.info/shakespeare" # shortcut URL
+shakespeare_url = "https://homl.info/shakespeare"  # shortcut URL
 filepath = tf.keras.utils.get_file("shakespeare.txt", shakespeare_url)
 
 with open(filepath) as f:
@@ -12,7 +12,7 @@ with open(filepath) as f:
 tokenizer = tf.keras.preprocessing.text.Tokenizer(char_level=True)
 tokenizer.fit_on_texts([shakespeare_text])
 
-max_id = len(tokenizer.word_index) # number of distinct characters
+max_id = len(tokenizer.word_index)  # number of distinct characters
 
 [encoded] = np.array(tokenizer.texts_to_sequences([shakespeare_text])) - 1
 dataset_size = encoded.shape[0]
@@ -21,7 +21,7 @@ train_size = dataset_size * 90 // 100
 dataset = tf.data.Dataset.from_tensor_slices(encoded[:train_size])
 
 n_steps = 100
-window_length = n_steps + 1 # target = input shifted 1 character ahead
+window_length = n_steps + 1  # target = input shifted 1 character ahead
 dataset = dataset.window(window_length, shift=1, drop_remainder=True)
 
 dataset = dataset.flat_map(lambda window: window.batch(window_length))
@@ -42,15 +42,15 @@ model = keras.models.Sequential([
                         dropout=0.2, recurrent_dropout=0.2),
        keras.layers.GRU(128, return_sequences=True,
                         dropout=0.2, recurrent_dropout=0.2),
-       keras.layers.TimeDistributed(keras.layers.Dense(max_id,
-                                                    activation="softmax"))])
+       keras.layers.TimeDistributed(keras.layers.Dense(max_id, activation="softmax"))])
 model.compile(loss="sparse_categorical_crossentropy", optimizer="adam")
 history = model.fit(dataset, epochs=1)
 
 
 def preprocess(texts):
-       X = np.array(tokenizer.texts_to_sequences(texts)) - 1
-       return tf.one_hot(X, max_id)
+    X = np.array(tokenizer.texts_to_sequences(texts)) - 1
+    return tf.one_hot(X, max_id)
+
 
 # test
 X_new = preprocess(["How are yo"])
