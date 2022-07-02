eb1e8ce7ecc630101f4df9cae45753f74c847dcb
paulsok <paul.s.falke@gmail.com>
2022-07-02 22:31:16 +0500

    sentiment analysis

--- nlp_rnn.py
+++ nlp_rnn.py
@@ -1,6 +1,8 @@
 import keras
 import numpy as np
 import tensorflow as tf
+from collections import Counter
+import tensorflow_datasets as tfds
 
 
 shakespeare_url = "https://homl.info/shakespeare"  # shortcut URL
@@ -100,3 +102,51 @@ class ResetStatesCallback(keras.callbacks.Callback):
 
 model.compile(loss="sparse_categorical_crossentropy", optimizer="adam")
 model.fit(dataset, epochs=50, callbacks=[ResetStatesCallback()])
+
+# sentiment analysis
+(X_train, y_train), (X_test, y_test) = keras.datasets.imdb.load_data()
+word_index = keras.datasets.imdb.get_word_index()
+
+id_to_word = {id_ + 3: word for word, id_ in word_index.items()}
+for id_, token in enumerate(("<pad>", "<sos>", "<unk>")):
+    id_to_word[id_] = token
+
+datasets, info = tfds.load("imdb_reviews", as_supervised=True, with_info=True)
+train_size = info.splits["train"].num_examples
+
+def preprocess(X_batch, y_batch):
+    X_batch = tf.strings.substr(X_batch, 0, 300)
+    X_batch = tf.strings.regex_replace(X_batch, b"<br\\s*/?>", b" ")
+    X_batch = tf.strings.regex_replace(X_batch, b"[^a-zA-Z']", b" ")
+    X_batch = tf.strings.split(X_batch)
+    return X_batch.to_tensor(default_value=b"<pad>"), y_batch
+
+vocabulary = Counter()
+for X_batch, y_batch in datasets["train"].batch(32).map(preprocess):
+    for review in X_batch:
+        vocabulary.update(list(review.numpy()))
+
+vocab_size = 10000
+truncated_vocabulary = [word for word, count in vocabulary.most_common()[:vocab_size]]
+
+
+words = tf.constant(truncated_vocabulary)
+word_ids = tf.range(len(truncated_vocabulary), dtype=tf.int64)
+vocab_init = tf.lookup.KeyValueTensorInitializer(words, word_ids)
+num_oov_buckets = 1000
+table = tf.lookup.StaticVocabularyTable(vocab_init, num_oov_buckets)
+
+def encode_words(X_batch, y_batch):
+    return table.lookup(X_batch), y_batch
+
+train_set = datasets["train"].batch(32).map(preprocess)
+train_set = train_set.map(encode_words).prefetch(1)
+
+embed_size = 128
+model = keras.models.Sequential([keras.layers.Embedding(vocab_size + num_oov_buckets, embed_size, input_shape=[None]),
+        keras.layers.GRU(128, return_sequences=True),
+        keras.layers.GRU(128),
+        keras.layers.Dense(1, activation="sigmoid")])
+
+model.compile(loss="binary_crossentropy", optimizer="adam", metrics=["accuracy"])
+history = model.fit(train_set, epochs=5)
