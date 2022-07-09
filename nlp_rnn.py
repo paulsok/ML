16d2be8e0051a7cb7499a34fad17b1fc0a1d7ffd
paulsok <paul.s.falke@gmail.com>
2022-07-09 19:26:33 +0500

    pep refactoring

--- nlp_rnn.py
+++ nlp_rnn.py
@@ -96,6 +96,7 @@ model = keras.models.Sequential([
         keras.layers.GRU(128, return_sequences=True, stateful=True, dropout=0.2, recurrent_dropout=0.2),
         keras.layers.TimeDistributed(keras.layers.Dense(max_id, activation="softmax"))])
 
+
 class ResetStatesCallback(keras.callbacks.Callback):
     def on_epoch_begin(self, epoch, logs):
         self.model.reset_states()
@@ -115,12 +116,14 @@ for id_, token in enumerate(("<pad>", "<sos>", "<unk>")):
 datasets, info = tfds.load("imdb_reviews", as_supervised=True, with_info=True)
 train_size = info.splits["train"].num_examples
 
+
 def preprocess(X_batch, y_batch):
     X_batch = tf.strings.substr(X_batch, 0, 300)
     X_batch = tf.strings.regex_replace(X_batch, b"<br\\s*/?>", b" ")
     X_batch = tf.strings.regex_replace(X_batch, b"[^a-zA-Z']", b" ")
     X_batch = tf.strings.split(X_batch)
     return X_batch.to_tensor(default_value=b"<pad>"), y_batch
+
 
 vocabulary = Counter()
 for X_batch, y_batch in datasets["train"].batch(32).map(preprocess):
@@ -137,17 +140,19 @@ vocab_init = tf.lookup.KeyValueTensorInitializer(words, word_ids)
 num_oov_buckets = 1000
 table = tf.lookup.StaticVocabularyTable(vocab_init, num_oov_buckets)
 
+
 def encode_words(X_batch, y_batch):
     return table.lookup(X_batch), y_batch
+
 
 train_set = datasets["train"].batch(32).map(preprocess)
 train_set = train_set.map(encode_words).prefetch(1)
 
 embed_size = 128
 model = keras.models.Sequential([keras.layers.Embedding(vocab_size + num_oov_buckets, embed_size, input_shape=[None]),
-        keras.layers.GRU(128, return_sequences=True),
-        keras.layers.GRU(128),
-        keras.layers.Dense(1, activation="sigmoid")])
+                                 keras.layers.GRU(128, return_sequences=True),
+                                 keras.layers.GRU(128),
+                                 keras.layers.Dense(1, activation="sigmoid")])
 
 model.compile(loss="binary_crossentropy", optimizer="adam", metrics=["accuracy"])
 history = model.fit(train_set, epochs=5)
@@ -166,12 +171,12 @@ model = keras.Model(inputs=[inputs], outputs=[outputs])
 # Reusing Pretrained Embeddings
 model = keras.Sequential([
     hub.KerasLayer("https://tfhub.dev/google/tf2-preview/nnlm-en-dim50/1",
-                    dtype=tf.string, input_shape=[], output_shape=[50]),
+                   dtype=tf.string, input_shape=[], output_shape=[50]),
     keras.layers.Dense(128, activation="relu"),
     keras.layers.Dense(1, activation="sigmoid")
 ])
 model.compile(loss="binary_crossentropy", optimizer="adam",
-                metrics=["accuracy"])
+              metrics=["accuracy"])
 
 datasets, info = tfds.load("imdb_reviews", as_supervised=True, with_info=True)
 train_size = info.splits["train"].num_examples
