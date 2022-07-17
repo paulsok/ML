8a259c250d778e418d7733121af258f566b483db
paulsok <paul.s.falke@gmail.com>
2022-07-17 22:54:45 +0500

    bert model: 17.07.22

--- tf/bert.py
+++ tf/bert.py
@@ -58,3 +58,136 @@ for text_batch, label_batch in train_ds.take(1):
         print(f'Review: {text_batch.numpy()[i]}')
         label = label_batch.numpy()[i]
         print(f'Label : {label} ({class_names[label]})')
+
+bert_preprocess_model = hub.KerasLayer(tfhub_handle_preprocess)
+
+text_test = ['this is such an amazing movie!']
+text_preprocessed = bert_preprocess_model(text_test)
+
+print(f'Keys       : {list(text_preprocessed.keys())}')
+print(f'Shape      : {text_preprocessed["input_word_ids"].shape}')
+print(f'Word Ids   : {text_preprocessed["input_word_ids"][0, :12]}')
+print(f'Input Mask : {text_preprocessed["input_mask"][0, :12]}')
+print(f'Type Ids   : {text_preprocessed["input_type_ids"][0, :12]}')
+
+bert_model = hub.KerasLayer(tfhub_handle_encoder)
+
+bert_results = bert_model(text_preprocessed)
+
+print(f'Loaded BERT: {tfhub_handle_encoder}')
+print(f'Pooled Outputs Shape:{bert_results["pooled_output"].shape}')
+print(f'Pooled Outputs Values:{bert_results["pooled_output"][0, :12]}')
+print(f'Sequence Outputs Shape:{bert_results["sequence_output"].shape}')
+print(f'Sequence Outputs Values:{bert_results["sequence_output"][0, :12]}')
+
+def build_classifier_model():
+    text_input = tf.keras.layers.Input(shape=(), dtype=tf.string, name='text')
+    preprocessing_layer = hub.KerasLayer(tfhub_handle_preprocess, name='preprocessing')
+    encoder_inputs = preprocessing_layer(text_input)
+    encoder = hub.KerasLayer(tfhub_handle_encoder, trainable=True, name='BERT_encoder')
+    outputs = encoder(encoder_inputs)
+    net = outputs['pooled_output']
+    net = tf.keras.layers.Dropout(0.1)(net)
+    net = tf.keras.layers.Dense(1, activation=None, name='classifier')(net)
+    return tf.keras.Model(text_input, net)
+
+classifier_model = build_classifier_model()
+bert_raw_result = classifier_model(tf.constant(text_test))
+print(tf.sigmoid(bert_raw_result))
+
+tf.keras.utils.plot_model(classifier_model)
+
+loss = tf.keras.losses.BinaryCrossentropy(from_logits=True)
+metrics = tf.metrics.BinaryAccuracy()
+
+epochs = 5
+steps_per_epoch = tf.data.experimental.cardinality(train_ds).numpy()
+num_train_steps = steps_per_epoch * epochs
+num_warmup_steps = int(0.1*num_train_steps)
+
+init_lr = 3e-5
+optimizer = optimization.create_optimizer(init_lr=init_lr,
+                                          num_train_steps=num_train_steps,
+                                          num_warmup_steps=num_warmup_steps,
+                                          optimizer_type='adamw')
+
+classifier_model.compile(optimizer=optimizer,
+                         loss=loss,
+                         metrics=metrics)
+
+print(f'Training model with {tfhub_handle_encoder}')
+history = classifier_model.fit(x=train_ds,
+                               validation_data=val_ds,
+                               epochs=epochs)
+
+loss, accuracy = classifier_model.evaluate(test_ds)
+
+print(f'Loss: {loss}')
+print(f'Accuracy: {accuracy}')
+
+history_dict = history.history
+print(history_dict.keys())
+
+acc = history_dict['binary_accuracy']
+val_acc = history_dict['val_binary_accuracy']
+loss = history_dict['loss']
+val_loss = history_dict['val_loss']
+
+epochs = range(1, len(acc) + 1)
+fig = plt.figure(figsize=(10, 6))
+fig.tight_layout()
+
+plt.subplot(2, 1, 1)
+# r is for "solid red line"
+plt.plot(epochs, loss, 'r', label='Training loss')
+# b is for "solid blue line"
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+# plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+
+plt.subplot(2, 1, 2)
+plt.plot(epochs, acc, 'r', label='Training acc')
+plt.plot(epochs, val_acc, 'b', label='Validation acc')
+plt.title('Training and validation accuracy')
+plt.xlabel('Epochs')
+plt.ylabel('Accuracy')
+plt.legend(loc='lower right')
+
+dataset_name = 'imdb'
+saved_model_path = './{}_bert'.format(dataset_name.replace('/', '_'))
+
+classifier_model.save(saved_model_path, include_optimizer=False)
+reloaded_model = tf.saved_model.load(saved_model_path)
+
+def print_my_examples(inputs, results):
+  result_for_printing = \
+    [f'input: {inputs[i]:<30} : score: {results[i][0]:.6f}'
+                         for i in range(len(inputs))]
+  print(*result_for_printing, sep='\n')
+  print()
+
+
+examples = [
+    'this is such an amazing movie!',  # this is the same sentence tried earlier
+    'The movie was great!',
+    'The movie was meh.',
+    'The movie was okish.',
+    'The movie was terrible...'
+]
+
+reloaded_results = tf.sigmoid(reloaded_model(tf.constant(examples)))
+original_results = tf.sigmoid(classifier_model(tf.constant(examples)))
+
+print('Results from the saved model:')
+print_my_examples(examples, reloaded_results)
+print('Results from the model in memory:')
+print_my_examples(examples, original_results)
+
+serving_results = reloaded_model \
+            .signatures['serving_default'](tf.constant(examples))
+
+serving_results = tf.sigmoid(serving_results['classifier'])
+
+print_my_examples(examples, serving_results)
