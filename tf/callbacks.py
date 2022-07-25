8956477c82f6134ba5087fd6ce3c02a69998c735
Pavel Sokolov <paul.s.falke@gmail.com>
2022-07-25 23:32:15 +0500

    playground with tf callbacks

---
+++ tf/callbacks.py
@@ -0,0 +1,101 @@
+import tensorflow as tf
+from tensorflow import keras
+
+
+# Define the Keras model to add callbacks to
+def get_model():
+    model = keras.Sequential()
+    model.add(keras.layers.Dense(1, input_dim=784))
+    model.compile(
+        optimizer=keras.optimizers.RMSprop(learning_rate=0.1),
+        loss="mean_squared_error",
+        metrics=["mean_absolute_error"],
+    )
+    return model
+
+
+# Load example MNIST data and pre-process it
+(x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
+x_train = x_train.reshape(-1, 784).astype("float32") / 255.0
+x_test = x_test.reshape(-1, 784).astype("float32") / 255.0
+
+# Limit the data to 1000 samples
+x_train = x_train[:1000]
+y_train = y_train[:1000]
+x_test = x_test[:1000]
+y_test = y_test[:1000]
+
+
+class CustomCallback(keras.callbacks.Callback):
+    def on_train_begin(self, logs=None):
+        keys = list(logs.keys())
+        print("Starting training; got log keys: {}".format(keys))
+
+    def on_train_end(self, logs=None):
+        keys = list(logs.keys())
+        print("Stop training; got log keys: {}".format(keys))
+
+    def on_epoch_begin(self, epoch, logs=None):
+        keys = list(logs.keys())
+        print("Start epoch {} of training; got log keys: {}".format(epoch, keys))
+
+    def on_epoch_end(self, epoch, logs=None):
+        keys = list(logs.keys())
+        print("End epoch {} of training; got log keys: {}".format(epoch, keys))
+
+    def on_test_begin(self, logs=None):
+        keys = list(logs.keys())
+        print("Start testing; got log keys: {}".format(keys))
+
+    def on_test_end(self, logs=None):
+        keys = list(logs.keys())
+        print("Stop testing; got log keys: {}".format(keys))
+
+    def on_predict_begin(self, logs=None):
+        keys = list(logs.keys())
+        print("Start predicting; got log keys: {}".format(keys))
+
+    def on_predict_end(self, logs=None):
+        keys = list(logs.keys())
+        print("Stop predicting; got log keys: {}".format(keys))
+
+    def on_train_batch_begin(self, batch, logs=None):
+        keys = list(logs.keys())
+        print("...Training: start of batch {}; got log keys: {}".format(batch, keys))
+
+    def on_train_batch_end(self, batch, logs=None):
+        keys = list(logs.keys())
+        print("...Training: end of batch {}; got log keys: {}".format(batch, keys))
+
+    def on_test_batch_begin(self, batch, logs=None):
+        keys = list(logs.keys())
+        print("...Evaluating: start of batch {}; got log keys: {}".format(batch, keys))
+
+    def on_test_batch_end(self, batch, logs=None):
+        keys = list(logs.keys())
+        print("...Evaluating: end of batch {}; got log keys: {}".format(batch, keys))
+
+    def on_predict_batch_begin(self, batch, logs=None):
+        keys = list(logs.keys())
+        print("...Predicting: start of batch {}; got log keys: {}".format(batch, keys))
+
+    def on_predict_batch_end(self, batch, logs=None):
+        keys = list(logs.keys())
+        print("...Predicting: end of batch {}; got log keys: {}".format(batch, keys))
+
+model = get_model()
+model.fit(
+    x_train,
+    y_train,
+    batch_size=128,
+    epochs=1,
+    verbose=0,
+    validation_split=0.5,
+    callbacks=[CustomCallback()],
+)
+
+res = model.evaluate(
+    x_test, y_test, batch_size=128, verbose=0, callbacks=[CustomCallback()]
+)
+
+res = model.predict(x_test, batch_size=128, callbacks=[CustomCallback()])
